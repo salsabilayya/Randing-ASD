c45ee31a05fa154a6b695dbaa5ecd5e7a2309b29
Tsurayya Salsabila <122155316+salsabilayya@users.noreply.github.com>
2023-01-09 23:18:23 +0800

    Update PostTest_ASD_1.py

--- PostTest_ASD_1.py
+++ PostTest_ASD_1.py
@@ -1,50 +1,39 @@
-def mergeSort(list):
-    list_length = len(list)
-    if list_length == 1:
-        return list
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
 
-    mid_point = list_length // 2
-    ngiri_partition = mergeSort(list[:mid_point])
-    nganan_partition = mergeSort(list[mid_point:])
-    return merge(ngiri_partition, nganan_partition)
+        mergeSort(left)
+        mergeSort(right)
 
-def merge(ngiri, nganan):
-    output = []
-    i = j = 0
-    while i < len(ngiri) and j < len(nganan):
-        if isinstance(ngiri[i], str) and isinstance(nganan[j], str):
-            if ngiri[i] < nganan[j]:
-                output.append(ngiri[i])
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left [i] < right [j]:
+                arr[k] = left[i]
                 i += 1
             else:
-                output.append(nganan[j])
+                arr[k]=right[j]
                 j += 1
-        elif isinstance(ngiri[i], str):
-            output.append(ngiri[i])
+            k += 1
+        while i < len(left):
+            arr[k] = left[i]
             i += 1
-        elif isinstance(nganan[j], str):
-            output.append(nganan[j])
+            k += 1
+        while j < len(right):
+            arr[k] = right[j]
             j += 1
-        else:
-            if ngiri[i] < nganan[j]:
-                output.append(ngiri[i])
-                i += 1
-            else:
-                output.append(nganan[j])
-                j += 1
-    output.extend(ngiri[i:])
-    output.extend(nganan[j:])
+            k += 1
 
-    return output
+def list(arr):
+    for i in range(len(arr)):
+        print(arr[i], end = " ")
+    print()
 
-def pisah(listnya):
-    for i in listnya:
-        tampungan.append(i)
-
-listacak = [12, 1, [22, 3, [8, 14]], 2, 6, [11], 90]
-tampungan = []
-print("List awal sebelum diurutkan ",listacak)
-pisah(listacak)
-print("List setelah diurutkan ",tampungan)
-sorted_list = mergeSort(tampungan)
-print("Hasil akhir list ",sorted_list)
+if __name__ == "__main__":
+    arr = [12, 1, 22, 3, 8, 14, 2, 6, 11 , 90]
+    print("List sebelum diurutkan: ", arr)
+    mergeSort(arr)
+    print ("List setelah diurutkan:", end = "\n")
+    list(arr)
